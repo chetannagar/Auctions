69d93665cef3ed166f32d590a035476108f0d4c3
Chetan Nagar <51130466+chetannagar@users.noreply.github.com>
2024-03-09 03:27:03 +1100

    Add project files.

---
+++ Auctions/Data/Services/ListingService.cs
@@ -0,0 +1,39 @@
+﻿using Auctions.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Auctions.Data.Services
+{
+    public class ListingService : IListingService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ListingService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Add(Listing listing)
+        {
+            _context.Listings.Add(listing);
+            await _context.SaveChangesAsync();
+        }
+
+        public IQueryable<Listing> GetAll()
+        {
+            var applicationDbContext = _context.Listings.Include(l => l.User);
+            return applicationDbContext;
+        }
+
+        public async Task<Listing> GetById(int? id)
+        {
+            var listing = await _context.Listings
+                .Include(l => l.User)
+                .Include(l => l.Comments)
+                .Include(l => l.Bids)
+                .ThenInclude(l => l.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            return listing;
+        }
+    }
+}
